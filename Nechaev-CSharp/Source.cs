35226a87b153bf675cf64b533c3fe2a054f10459
Levi_333 <leva.nechaev2008@gmail.com>
2026-03-16 12:47:53 +0300

    Работа на паре и практика 16.03.2026

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,69 +1,130 @@
 ﻿using System; // Системные классы и функции
-using System.Collections.Generic; // Коллекции, на подобие List, Dictionary и так далее
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.AccessControl; // Коллекции, на подобие List, Dictionary и так далее
 
 namespace Nechaev_CSharp // Пространство имён
 {
-    class Safe
+    enum Rarity
     {
-        private string contents;
-        private int pinCode;
+        Common = 0,
+        Uncommon = 1,
+        Epic = 2,
+        Legendary = 3,
+        Champion = 4
+    }
+    class Unit
+    {
+        public delegate string BattleCryDelegate();
+        public delegate string RarityCollorDelegate();
 
-        public int PinCode
+        private string unitName;
+        private int x, y;
+        private uint health, elixirCost, attackDamage;
+        private Rarity rarity;
+
+        public void Move(BattleCryDelegate battleCryDelegate, int x, int y)
         {
-            private get { return pinCode; }
-            init { pinCode = value; }
+            //ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            //processStartInfo.Arguments = "D:\\Nechaev\\Nechaev_CSharp\\Nechaev-CSharp\\niggers.mp3";
+            //processStartInfo.FileName = "cmd.exe";
+            //processStartInfo.UseShellExecute = true;
+
+            //Process process = new Process();
+            //process.StartInfo = processStartInfo;
+            //process.Start();
+
+            Console.WriteLine($"{unitName} кричит {battleCryDelegate.Invoke()}");
+            this.x = x;
+            this.y = y;
+            Process.Start("cmd.exe", "D:\\Nechaev\\Nechaev_CSharp\\Nechaev-CSharp\\niggers.mp3");
         }
-        public string Contents
+        public void Attack(Unit other)
         {
-            get
+            other.TakeDamage(attackDamage);
+        }
+        public void TakeDamage(uint damage)
+        {
+            health -= damage;
+        }
+        public void ChangeCollor(RarityCollorDelegate rarityCollorDelegate, Rarity rarity)
+        {
+            Console.WriteLine($"Текущий цвет юнита {unitName}: {rarityCollorDelegate.Invoke()}");
+            this.rarity = rarity;
+            Console.WriteLine($"Изменённый цвет юнита {unitName}: {rarityCollorDelegate.Invoke()}");
+        }
+
+        public Unit(int x, int y, uint health, uint elixirCost, uint attackDamage, string unitName, Rarity rarity)
+        {
+            this.x = x;
+            this.y = y;
+            this.health = health;
+            this.elixirCost = elixirCost;
+            this.attackDamage = attackDamage;
+            this.unitName = unitName;
+            this.rarity = rarity;
+        }
+
+        public string Scream()
+        {
+            return "Hooog riideeeer!";
+        }
+        public string Cry()
+        {
+            return "Snickers";
+        }
+        public string GetCollor()
+        {
+            switch (rarity)
             {
-                Console.WriteLine("Введите пароль");
-                int tempPinCode = int.Parse(Console.ReadLine());
-                if (tempPinCode == pinCode) return contents;
-                else 
-                {
-                    Console.WriteLine("Отказано в доступе. Неверный пин-код.");
-                    return null;
-                }
+                case Rarity.Common:
+                    return "Голубой";
+                case Rarity.Uncommon:
+                    return "Оранжевый";
+                case Rarity.Epic:
+                    return "Фиолетовый";
+                case Rarity.Legendary:
+                    return "Призматический";
+                case Rarity.Champion:
+                    return "Золотой";
+                default:
+                    return "null";
             }
-            set
+        }
+        public string GetCollor2()
+        {
+            switch (rarity)
             {
-                Console.WriteLine("Введите пин-код");
-                int tempPinCode = int.Parse(Console.ReadLine());
-                if (tempPinCode == pinCode) contents += ", " + value;
-                else Console.WriteLine("Отказано в доступе. Неверный пин-код.");
+                case Rarity.Common:
+                    return "Голубой";
+                case Rarity.Uncommon:
+                    return "Оранжевый";
+                case Rarity.Epic:
+                    return "Фиолетовый";
+                case Rarity.Legendary:
+                    return "Призматический";
+                case Rarity.Champion:
+                    return "Золотой";
+                default:
+                    return "null";
             }
-        }
-        public Safe (int pinCode)
-        {
-            this.pinCode = pinCode;
-        }
-        public void ChangePinCode(int NewpinCode)
-        {
-            Console.WriteLine("Введите старый пин-код");
-            int tempPinCode = int.Parse(Console.ReadLine());
-            if (tempPinCode == this.pinCode) this.pinCode = NewpinCode;
-            else Console.WriteLine("Отказано в доступе. Неверный пин-код.");
         }
     }
     class Source
     {
+        
+
         static void Main(string[] args)
         {
-            Safe safe = new Safe(1488);
+            Unit hogRider = new Unit(0, 0, 2046, 4, 383, "Всадник на кабане", Rarity.Uncommon);
+            Unit skeletons = new Unit(0, 0, 89, 1, 89, "Скелеты", Rarity.Common);
 
-            Console.WriteLine("Добавление предмета");
-            safe.Contents = "Крутые очки";
-            Console.WriteLine("Просмотр предметов");
-            Console.WriteLine(safe.Contents);
-            Console.WriteLine("Изменение пароль");
-            safe.ChangePinCode(1337);
-            Console.WriteLine("Просмотр предметов");
-            Console.WriteLine(safe.Contents);
-            Console.WriteLine("Добавление предмета");
-            safe.Contents = "Супер крутые очки";
-            Console.WriteLine("Просмотр предметов");
-            Console.WriteLine(safe.Contents);
+            hogRider.ChangeCollor(hogRider.GetCollor, Rarity.Legendary);
+            skeletons.ChangeCollor(skeletons.GetCollor2, Rarity.Champion);
         }
     }
 }
+//		Практика
+// 1. Создать поле делегата у класса Unit, которое бы в дальнейшем принимало на себя задачу по смене цвета карты юнита. Например, у хог райдера - оранжевый, у скелетиков - голубой.
+// 2. Создать функцию смены цвета юнита, которая бы принимала объект делегата
+// 3. Создать две функции с типом возвращаемого значения string, которые бы в зависимости от поля rarity в конструкции switch, возвращали строку с цветом юнита.
