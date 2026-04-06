04551db249b85e262e72e45aea0f074d3f0b38a7
Levi_333 <leva.nechaev2008@gmail.com>
2026-04-06 14:43:22 +0300

    Практика с музыкальными инструментами (классная)

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,70 +1,107 @@
 ﻿using System; // Системные классы и функции
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Security.AccessControl; // Коллекции, на подобие List, Dictionary и так далее
+using System.Security.AccessControl;
+using System.Threading.Channels; // Коллекции, на подобие List, Dictionary и так далее
 
 namespace Nechaev_CSharp // Пространство имён
 {
-    class SadException : Exception
+    class Instrument
     {
-        private int[] array;
-        public SadException(int[] array)
+        private string model;
+        private float coast;
+        private bool isTune;
+        public string Model
         {
-            this.array = array;
+            get { return model; }
+            set { model = value; }
         }
-        public override string Message
+        public float Coast
         {
-            get { return $"Массив не инициализирован!"; }
+            get { return coast; }
+            set { coast = value; }
+        }
+        public bool IsTune
+        {
+            get { return isTune; }
+            set { isTune = value; }
+        }
+        public Instrument(string model, float coast, bool isTune)
+        {
+            this.model = model;
+            this.coast = coast;
+            this.isTune = isTune;
+        }
+
+        public void Play() => Console.WriteLine("Инструмент звучит");
+        public void Tune()
+        {
+            isTune = true;
+            Console.WriteLine("Инструмент настроен");
+        }
+    }
+    class Gutar : Instrument
+    {
+        private uint stringCount;
+        private bool isElectronic;
+        public uint StringCount
+        {
+            get { return stringCount; }
+            set { stringCount = value; }
+        }
+        public bool IsElectronic
+        {
+            get { return isElectronic; }
+            set { isElectronic = value; }
+        }
+        public Gutar(uint stringCount, bool isElectronic, string model, float coast, bool isTune) : base(model, coast, isTune)
+        {
+            this.stringCount = stringCount;
+            this.isElectronic = isElectronic;
+        }
+        public void PerformOnTheStreet()
+        {
+            if (IsTune) Console.WriteLine("Вы брынчите на улице. В будущем вы строчаетесь");
+            else Console.WriteLine("Вы позоритесь на всю улицу. В будущем вы строчаетесь");
+        }
+    }
+    class Piano : Instrument
+    {
+        private uint fretsCount;
+        private bool isElectronic;
+        public uint FretsCount
+        {
+            get { return fretsCount; }
+            set { fretsCount = value; }
+        }
+        public bool IsElectronic
+        {
+            get { return isElectronic; }
+            set { isElectronic = value; }
+        }
+        public Piano(uint fretsCount, bool isElectronic, string model, float coast, bool isTune) : base(model, coast, isTune)
+        {
+            this.fretsCount = fretsCount;
+            this.isElectronic = isElectronic;
+        }
+        public void PerformInARestaurant()
+        {
+            if (IsTune) Console.WriteLine("Вы в красивом костюме выступаете в элитном ресторане");
+            else Console.WriteLine("Вы позоритель на глазах у элиты");
         }
     }
 
     class Source
     {
-        static int Divide(int[]? array, int i, int j)
-        {
-            if (array == null) throw new SadException(array);
-            return array[i] / array[j];
-        }
-
         static void Main(string[] args)
         {
-            int[]? array1 = null;
-            int[]? array2 = { 2, 0 };
-            int[]? array3 = { 4, 5 };
-
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
-                    if (i == 0) Console.WriteLine($"Деление выполено: {Divide(array1, 0, 1)}");
-                    else if (i == 1) Console.WriteLine($"Деление выполено: {Divide(array2, 0, 1)}");
-                    else if (i == 2) Console.WriteLine($"Деление выполено: {Divide(array3, 0, 2)}");
-                }
-                catch (DivideByZeroException zeroExeption)
-                {
-                    Console.WriteLine(zeroExeption.Message);
-                }
-                //catch (NullReferenceException nullExeption)
-                //{
-                //    Console.WriteLine(nullExeption.Message);
-                //}
-                catch (IndexOutOfRangeException indexExeption)
-                {
-                    Console.WriteLine(indexExeption.Message);
-                }
-                catch (SadException divideException)
-                {
-                    Console.WriteLine(divideException.Message);
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("Не смотря на все исключение деление отработало\n");
-                }
-            }
+            Gutar gutar = new Gutar(6, false, "Jet", 15000, false);
+            gutar.PerformOnTheStreet();
+            gutar.Tune();
+            gutar.PerformOnTheStreet();
+            Instrument newGutar = gutar;
+            newGutar.Coast = 3000;
+            if (newGutar is Gutar) Console.WriteLine("Вы сторчались.");
         }
     }
 }
