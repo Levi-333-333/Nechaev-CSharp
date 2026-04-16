504fac86de7844e30f3bcf8709fd7fd1fd6a397f
Levi_333 <leva.nechaev2008@gmail.com>
2026-04-16 22:02:09 +0300

    ДЗ 16.04.2026

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,30 +1,53 @@
 ﻿using System;
 using System.Diagnostics;
-using Departament_namespace;
-using Worker_namespace;
+
+// Интерфейс, определяющий функционал для движимых объектов.
+// ОНИ МОГУТ БЫТЬ РАЗНОЙ ПРИРОДЫ!
+public interface IAnimal
+{
+    public void Move();
+    public void CollisionDetect() // пример-функция столкновения
+    {
+        Console.WriteLine("Объект столкнулся с другим объектом.");
+    }
+    public string Name { get; }
+    public void PrintName() => Console.WriteLine($"Животное кличас {Name}");
+}
+
+class Racoon : IAnimal
+{
+    private string name;
+    public Racoon(string name) => this.name = name;
+    public string Name { get { return name; } }
+    public void Move() => Console.WriteLine("Енот движется");
+    public void Pet() => Console.WriteLine($"Вы гладите {name}");
+}
 
 class Source
 {
     static void Main(string[] args)
     {
-        Departament it = new Departament("IT");
-        Departament marketing = new Departament("Marketing");
+        IAnimal[] zoo = new IAnimal[] { new Racoon("Бандит"), new Racoon("Ракета"), new Racoon("Болт") };
 
-        Worker kira = new Worker("Kirill", 6000, 6);
-
-        it.AddWorker(new Worker("Petya", 6000, 6));
-        it.AddWorker(kira);
-        it.AddWorker(new Worker("Vasily", 6000, 6));
-
-        marketing.AddWorker(new Worker("Anastasiya", 3002, 2));
-        marketing.AddWorker(new Worker("Ivan", 3002, 2));
-        marketing.AddWorker(new Worker("Vasilisa", 3002, 2));
-
-        it.PrintWorkers();
-        marketing.PrintWorkers();
-
-        it.FireWorker(kira);
-        Console.WriteLine();
-        it.PrintWorkers();
+        bool isSucsess = false;
+        Console.Write("Введите имя енота, которого хотите погладить: ");
+        string userInput = Console.ReadLine();
+        foreach (Racoon racoon in zoo)
+        {
+            if (racoon.Name == userInput)
+            {
+                racoon.Pet();
+                isSucsess = true;
+                break;
+            }
+        }
+        if (!isSucsess) Console.WriteLine("Нет такого енота в зоопарке");
     }
 }
+
+//      Практика
+// 1. Дополнить программу новым интерфейсом функционала клички. В интерфейсе должны быть поле имени с определенными get и set свойствами и функция, которая бы в консоль выводила кличку животного в консоли.
+// 2. В Main создать массив зоопарка, который должен хранить в себе объекты интерфейсов.
+// 3. Дополнить классы животных функцией Pet(), которая выводит в консоль о том, что пользователь гладит животное + имя
+// 4. Просить пользователя ввести имя животного, которое он хочет погладить.
+// 5. В цикле перебирать каждое животное и сравнивать его имя со вводом пользователя. Если есть совпадение, то вызывать функцию Pet() от перечисляемого объекта.
