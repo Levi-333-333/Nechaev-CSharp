88b61fadf5b672c7e55b5bb7827f1fcf3b89bb46
Levi_333 <leva.nechaev2008@gmail.com>
2026-04-16 15:01:52 +0300

    Практика 16.04.2026

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,102 +1,21 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Numerics;
 
-abstract class OutputDevice
-{
-    public abstract void Move();
-    public abstract void ShowData();
-    public abstract void TurnOff();
-    public abstract string Model { get; set; }
-}
-
-public interface IInputDevice
-{
-    public void Move();
-    public void EnterData();
-    public void TurnOff();
-}
-
-class ComputerMouse : IInputDevice
-{
-    public void Move() 
-    {
-        Console.WriteLine("Вы подвигали мышку.");
+struct Point {
+    float x, y;
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
     }
-    public void EnterData()
-    {
-        Console.WriteLine("Вы кликнули кнопкой мыши.");
-    }
-    public void TurnOff()
-    {
-        Console.WriteLine("Вы выключили мышь.");
-    }
-}
-
-class Monitor : OutputDevice
-{
-    public override void Move()
-    {
-        Console.WriteLine("Монитор подвигали.");
-    }
-    public override void ShowData()
-    {
-        Console.WriteLine("Монитор выводит экран.");
-    }
-    public override void TurnOff()
-    {
-        Console.WriteLine("Монитор выключен.");
-    }
-
-    public override string Model
-    {
-        get { return model; }
-        set { model = value; }
-    }
-
-    private string model;
-}
-class Printer : OutputDevice
-{
-    public override void Move()
-    {
-        Console.WriteLine("Принтер подвигали.");
-    }
-    public override void ShowData()
-    {
-        Console.WriteLine("Принтер печатает.");
-    }
-    public override void TurnOff()
-    {
-        Console.WriteLine("Принтер выключен.");
-    }
-
-    public override string Model
-    {
-        get { return model; }
-        set { model = value; }
-    }
-
-    private string model;
+    public void Draw() => Console.WriteLine($"Всем привет я точка ({x}:{y})");
 }
 
 class Source
 {
     static void Main(string[] args)
     {
-        Monitor monitor = new Monitor();
-        monitor.Model = "ViewSonic";
-
-        Printer printer = new Printer();
-        printer.Model = "HP";
-
-        OutputDevice[] devices = new OutputDevice[] { monitor, printer };
-
-        Console.Write("Каким девайсом вы хотите воспользоваться? (ViewSonic/HP)");
-        string userInput = Console.ReadLine();
-
-        for (int i = 0; i < devices.Length; i++)
-        {
-            if (devices[i].Model == userInput) devices[i].ShowData();
-        }
+        Point p = new Point(3, 6);
+        p.Draw();
     }
 }
