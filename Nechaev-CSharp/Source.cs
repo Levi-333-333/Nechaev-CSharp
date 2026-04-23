fdc03e681af67a6ee2214c7c8e927b7803ecea70
Levi_333 <leva.nechaev2008@gmail.com>
2026-04-23 13:57:30 +0300

    23.04.2026 Работа на паре

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,65 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
  
-enum OrderStatus
-{
-    OrderCreated = 1,
-    OrderPaid,
-    OrderSent,
-    OrderDelevered,
-    OrderCanseled
-}
-
-class Order
-{
-    private uint id;
-    private OrderStatus status;
-    public Order(uint id) 
-    { 
-        this.id = id; 
-        status = OrderStatus.OrderCreated;
-    }
-    public void ChangeStatus(OrderStatus newStatus) => status = newStatus;
-    public void PrintInfo()
-    {
-        string currentOrderStatus;
-        switch (status)
-        {
-            case OrderStatus.OrderCreated:
-                currentOrderStatus = "Заказ создан";
-                break;
-            case OrderStatus.OrderPaid:
-                currentOrderStatus = "Заказ оплачен";
-                break;
-            case OrderStatus.OrderSent:
-                currentOrderStatus = "Заказ отправлен";
-                break;
-            case OrderStatus.OrderDelevered:
-                currentOrderStatus = "Заказ доставлен";
-                break;
-            case OrderStatus.OrderCanseled:
-                currentOrderStatus = "Заказ отменён";
-                break;
-            default:
-                currentOrderStatus = "Неизвестно";
-                break;
-        }
-        Console.WriteLine($"Номер заказа {id}: статус заказа {currentOrderStatus}");
-    }
-
 class Source
 {
     string whoIsMe = "я редиска";
     static void Main(string[] args)
     {
-        Order order = new Order(1);
+        Queue<string> nameQueue = new Queue<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
+        nameQueue.Enqueue("Родин");
+        nameQueue.Dequeue(); // Давид убрался
 
-        order.PrintInfo();
-        order.ChangeStatus(OrderStatus.OrderPaid);
-        order.PrintInfo();
-        order.ChangeStatus(OrderStatus.OrderSent);
-        order.PrintInfo();
-        order.ChangeStatus(OrderStatus.OrderDelevered);
-        order.PrintInfo();
+        Stack<string> nameStack = new Stack<string>(new[] { "Давид", "Лев", "Рия", "Тимур", "Филипп", "Расул", "Андрей", "Тимур Тимурович" });
+        nameStack.Push("Родин");
+        string poppedItem = nameStack.Pop();
+        Console.WriteLine(poppedItem); // Родин
+        string tryPoppedItem = "";
+        if (nameStack.TryPop(out tryPoppedItem)) Console.WriteLine("Элемент успешно изъятирован.");
+        else Console.WriteLine("Провал изъятирования.");
+
+        Dictionary<string, uint> studentsDict = new Dictionary<string, uint>();
+        studentsDict.Add("Давид", 2);
+        studentsDict.Add("Рия", 5);
+        studentsDict.Add("Лев", 2);
+        studentsDict.Add("Тимур", 16);
+        studentsDict.Add("Филипп", 21);
+        studentsDict.Add("Расул", 7);
+        studentsDict.Add("Андрей", 0);
+        studentsDict.Add("Тимур Тимурович", 67);
+        studentsDict["Родин"] = 666;
+
+        foreach (KeyValuePair<string, uint> item in studentsDict)
+        {
+            Console.WriteLine($"Студент {item.Key} с оценкой {item.Value}");
+        }
     }
 }
