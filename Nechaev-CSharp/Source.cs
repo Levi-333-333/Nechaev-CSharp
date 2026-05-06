6c2e73d99872ddc34e11b48b1c048645b41238a2
Levi_333 <leva.nechaev2008@gmail.com>
2026-05-06 13:52:08 +0300

    Практика и работа на паре 06.05.2026

--- Nechaev-CSharp/Source.cs
+++ Nechaev-CSharp/Source.cs
@@ -1,45 +1,88 @@
 ﻿using System;
 using System.Diagnostics;
- 
-class Pixel
+using System.Numerics;
+
+class College
 {
-    private uint r, g, b, a;
-    public Pixel(uint r, uint g, uint b, uint a)
-    {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
-    }
-    public override string ToString()
-    {
-        return $"{r}, {g}, {b}, {a}";
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public College(int id, string? name)
+    {  
+        Id = id; 
+        Name = name; 
     }
 }
+class Student
+{
+    public int Id { get; set; }
+    public int CollegeId { get; set; }
+    public int VoenkomatId { get; set; }
+    public string Name { get; set; }
+    public uint Score { get; set; }
+
+    public Student(int id, int collegeId, int voenkomatId, string name, uint score)
+    {
+        Id = id;
+        CollegeId = collegeId;
+        VoenkomatId = voenkomatId;
+        Name = name;
+        Score = score;
+    }
+}
+class Voenkomat
+{
+    public string? Name { get; set; }
+    public int Id { get; set; }
+    public Voenkomat(int id, string? name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
 class Source
 {
-    string whoIsMe = "я редиска";
-    static void PixelWork(Pixel pixel)
-    {
-        Console.WriteLine($"Пиксель {pixel} работает...");
-        Thread.Sleep( 1000 );
-        Console.WriteLine("Пиксель отработал своё.");
-    }
     static void Main(string[] args)
     {
-        List<Pixel> pixels = new List<Pixel>(new[] { new Pixel(255, 255, 255, 255), new Pixel(134, 246, 54, 155), new Pixel(33, 65, 32, 1) });
-        pixels.AddRange(new[] { new Pixel(153, 64, 2, 6), new Pixel(23, 4, 56, 2), new Pixel(46, 35, 76, 255) });
-        foreach (Pixel pixel in pixels)
+        List<College> colleges = new List<College>
         {
-            Console.WriteLine(pixel);
-        }
-        
-        LinkedList<Pixel> list = new LinkedList<Pixel>(pixels);
-        list.AddFirst(new Pixel(34, 34, 34, 34));
-        for (var i = list.First; pixels != null;)
+            new College(0, "IT TOP"),
+            new College(1, "VOLSU"),
+            new College(2, "VSPU"),
+            new College(3, "ALABUGA")
+        };
+        List<Voenkomat> voenkomats = new List<Voenkomat>
         {
-            PixelWork(list.First.Value);
-            list.RemoveFirst();
+            new Voenkomat(0, "IT TOP Voenkomat"),
+            new Voenkomat(1, "Военный Комиссариат Советского, Ворошиловского и Кировского райнов"),
+            new Voenkomat(2, "Военкомат"),
+            new Voenkomat(3, "Военный Комиссариат Волгоградской Области"),
+            new Voenkomat(4, "Военкомат г. Волжский Волгоградской Области")
+        };
+
+        List<Student> students = new List<Student>
+        {
+            new Student(1, colleges[0].Id, voenkomats[2].Id, "Андрей", 10),
+            new Student(2, colleges[2].Id, voenkomats[0].Id, "Давид", 1),
+            new Student(3, colleges[1].Id, voenkomats[4].Id, "Рия", 5),
+            new Student(4, colleges[3].Id, voenkomats[1].Id, "Расул", 120000),
+            new Student(5, colleges[3].Id, voenkomats[4].Id, "Лев", 5),
+            new Student(6, colleges[2].Id, voenkomats[2].Id, "Алексей", 5),
+            new Student(7, colleges[1].Id, voenkomats[2].Id, "Максим", 0),
+            new Student(8, colleges[1].Id, voenkomats[3].Id, "Тимур", 4),
+            new Student(9, colleges[0].Id, voenkomats[1].Id, "Александр", 365),
+            new Student(10, colleges[0].Id, voenkomats[1].Id, "Филипп", 8),
+            new Student(11, colleges[0].Id, voenkomats[2].Id, "Тимур", 3),
+            new Student(12, colleges[3].Id, voenkomats[1].Id, "Владислав", 4),
+            new Student(13, colleges[2].Id, voenkomats[2].Id, "Максим", 3),
+            new Student(14, colleges[0].Id, voenkomats[1].Id, "Дмитрий", 6)
+        };
+
+        var voikomsJoinLINQ = from student in students join voenkomatJoin in voenkomats on student.VoenkomatId equals voenkomatJoin.Id select new { name = student.Name, voenkomatName = voenkomatJoin.Name };
+        foreach (var item in voikomsJoinLINQ)
+        {
+            Console.WriteLine($"Имя студента: {item.name}, привязан к военкомату {item.voenkomatName}");
+            
         }
     }
 }
